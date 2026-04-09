658ca70442f1c172cf49f2ac1d4cea0d56bc2a3b
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-04-09 08:53:11 -0300

    fix: update echo command implementation and change package version and license

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ client.on('ready', () => console.log('Client is ready!'))
 
 const commands = {
   ping: (msg) => msg.reply('pong'),
-  echo: (...msg) => msg.reply(msg.join(' ')),
-  news: (msg) => null, // fixme
+  echo: (msg, text) => msg.reply(text),
+  news: () => null, // fixme
 }
 
-client.on('message', (msg) => {
+client.on('message_create', (msg) => {
   if (msg.body.startsWith('/')) {
     const [command, ...args] = msg.body.slice(1).split(' ')
     const cmd = commands[command]
